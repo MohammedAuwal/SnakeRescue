de7b38d35b1c7cf8581b5d6b49b5d25653f4e6c3
MohammedAuwal <mohammedauwalnasara.com>
2026-06-19 13:46:16 +0100

    fix: wrap editor script in UNITY_EDITOR preprocessor directive to fix compilation error

--- Assets/Editor/AutoGenerateConfigs.cs
+++ Assets/Editor/AutoGenerateConfigs.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using SnakeRescue.Data;
@@ -39,3 +40,4 @@ public class AutoGenerateConfigs
         Debug.Log("Default Assets Generated Successfully.");
     }
 }
+#endif
